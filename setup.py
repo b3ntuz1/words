14395c74aff68343fab8dd723ce3ae954e0be294
tony <tonibalog@gmail.com>
2021-08-09 23:37:04 +0300

    [work_in_progress] setup database

--- setup.py
+++ setup.py
@@ -1,21 +1,38 @@
-import cruid
 from json import loads
+from os import remove
+from os.path import exists
+# видалити стару БД
+if exists("gamedata.db"):
+    remove("gamedata.db")
 
 
-names = cruid.DataBase()
+# імпорти мали б бути вище, але інакше не працює видалення файлу з БД
+from models import Words, GameData, Rankings, UsedWords
 
-names.delete("names")
-names.delete("rankings")
-names.delete("gamedata")
 
-names.create("names", {
-    "letter": "TEXT",
-    "words": "TEXT"
-})
+# підключитися до БД
+words = Words()
+gamedata = GameData()
+rankings = Rankings()
+used_words = UsedWords()
 
+# створити нові таблиці
+words.create_table()
+gamedata.create_table()
+rankings.create_table()
+used_words.create_table()
+
+# заповнити таблиці
+gamedata.last_user = "_"
+gamedata.current_letter = ""
+gamedata.words = ""
+gamedata.save()
+
+# завантажити дані із .json файлу
 with open("words.json", "r") as fh:
-    data = loads(fh.read())
+    raw_data = loads(fh.read())
 
-for key in data.keys():
-    names.insert("names", [key, data[key]])
-    # TODO: згенерувати список слів variants.txt
+# зберегти та закінчити роботу
+for k, v in sorted(raw_data.items()):
+    Words(letter=k, word_lists=v).save()
+    UsedWords(letter=k, word_lists="").save()
