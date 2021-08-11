788cf2f8e3e4e2f6d62e52119ddc2cb31ce68c9d
tony <tonibalog@gmail.com>
2021-08-11 18:52:35 +0300

    [text_fixes] скрипт став більш говірким)

--- setup.py
+++ setup.py
@@ -1,38 +1,47 @@
 from json import loads
 from os import remove
 from os.path import exists
-# видалити стару БД
-if exists("gamedata.db"):
-    remove("gamedata.db")
 
 
-# імпорти мали б бути вище, але інакше не працює видалення файлу з БД
-from models import Words, GameData, Rankings, UsedWords
+def main():
+    # видалити стару БД
+    if exists("gamedata.db"):
+        print("[setup] Remove old DB")
+        remove("gamedata.db")
+
+    # імпорти мали б бути вище, але інакше не працює видалення файлу з БД
+    from models import Words, GameData, Rankings, UsedWords
+
+    # підключитися до БД
+    print("[setup] Connecting...")
+    words = Words()
+    gamedata = GameData()
+    rankings = Rankings()
+    used_words = UsedWords()
+
+    # створити нові таблиці
+    words.create_table()
+    gamedata.create_table()
+    rankings.create_table()
+    used_words.create_table()
+
+    # заповнити таблиці
+    gamedata.last_user = "_"
+    gamedata.current_letter = ""
+    gamedata.words = ""
+    gamedata.save()
+
+    # завантажити дані із .json файлу
+    print("[setup] Read the json file")
+    with open("words.json", "r") as fh:
+        raw_data = loads(fh.read())
+
+    # зберегти та закінчити роботу
+    print("[setup] Filling DB")
+    for k, v in sorted(raw_data.items()):
+        Words(letter=k, word_lists=v).save()
+        UsedWords(letter=k, word_lists="").save()
 
 
-# підключитися до БД
-words = Words()
-gamedata = GameData()
-rankings = Rankings()
-used_words = UsedWords()
-
-# створити нові таблиці
-words.create_table()
-gamedata.create_table()
-rankings.create_table()
-used_words.create_table()
-
-# заповнити таблиці
-gamedata.last_user = "_"
-gamedata.current_letter = ""
-gamedata.words = ""
-gamedata.save()
-
-# завантажити дані із .json файлу
-with open("words.json", "r") as fh:
-    raw_data = loads(fh.read())
-
-# зберегти та закінчити роботу
-for k, v in sorted(raw_data.items()):
-    Words(letter=k, word_lists=v).save()
-    UsedWords(letter=k, word_lists="").save()
+if __name__ == "__main__":
+    main()
