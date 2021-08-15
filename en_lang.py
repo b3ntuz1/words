bbd665a3930435c7c7bedb92584d5de3f7a30c28
tony <tonibalog@gmail.com>
2021-08-15 21:52:51 +0300

    [develop] localization added

---
+++ en_lang.py
@@ -0,0 +1,6 @@
+# en_lang.py
+class TextsForGame:
+    user_cant_move = "{user} not found"
+    next_word_starts_with = "Next word should starts with {letter}"
+    wrong_answer = "Sorry you answer is wrong. Try again."
+    used_word = "Sorry but that word has already been used. Try another word, please."
