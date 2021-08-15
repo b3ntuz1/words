4c07f67ddb145c82df7d85958ec5fb69726cab3f
tony <tonibalog@gmail.com>
2021-08-15 23:55:41 +0300

    [develop] add new data

--- en_lang.py
+++ en_lang.py
@@ -4,3 +4,4 @@ class TextsForGame:
     next_word_starts_with = "Next word should starts with {letter}"
     wrong_answer = "Sorry you answer is wrong. Try again."
     used_word = "Sorry but that word has already been used. Try another word, please."
+    correct_answer = "Awesome. Next word should starts with letter {letter}. I know {count} variants."
